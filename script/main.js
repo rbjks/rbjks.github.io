e6b9a0e0366138b3c66080823614cacb128b1e08
dumbcoder7 <2017.aditya.raute@ves.ac.in>
2023-02-06 09:50:09 +0530

    New Approach

--- script/main.js
+++ script/main.js
@@ -1,30 +1,46 @@
 var prefLanguage = 'en';
 //https://stackoverflow.com/questions/16206322/how-to-get-js-variable-to-retain-value-after-page-refresh
 
-var englishElements = document.querySelectorAll('[lang = en]')
-var hindiElements = document.querySelectorAll('[lang = hi]')
+var translateElements = document.querySelectorAll('.translate')
+var htmlParent = document.querySelector('html')
+//var englishElements = document.querySelectorAll('.translate')
+//var hindiElements = document.querySelectorAll('[lang = hi]')
 
-window.onload= () => {
-    changelanguage(prefLanguage);
-}
+window.onload = () => {
+    console.log(prefLanguage);
+    if(prefLanguage != 'en')
+        changelanguage();
+ }
 
-function changelanguage(lang){
-    prefLanguage = lang;
-    if(prefLanguage == 'en'){
-        makeEnglish();
-    }
-    else{
-        makeHindi();
-    }
-    console.log('current pref ',lang)
+function changelanguage(){
+    prefLanguage = prefLanguage == 'en'? 'hi': 'en';
+    htmlParent.lang = prefLanguage;
+    translateElements.forEach(el => swap(el));
+    // prefLanguage = lang;
+    // if(prefLanguage == 'en'){
+    //     makeEnglish();
+    // }
+    // else{
+    //     makeHindi();
+    // }
+    // console.log('current pref ',lang)
 }
 
 function makeEnglish() {
-    hindiElements.forEach(el => el.style.display = "none");
-    englishElements.forEach(el => el.style.display = "inherit");
+    //hindiElements.forEach(el => el.style.display = "none");
+    //englishElements.forEach(el => el.style.display = "unset");
 }
 
 function makeHindi() {
-    hindiElements.forEach(el => el.style.display = "inherit");
-    englishElements.forEach(el => el.style.display = "none");
+    //hindiElements.forEach(el => el.style.display = "unset");
+    //console.log(englishElements);
+    //englishElements.forEach(el => el.style.display = "none");
+    //englishElements.forEach(el => swap(el));
+}
+
+function swap(el){
+    var i = el.ariaLabel;
+    el.ariaLabel = el.innerText;
+    el.innerText = i;
+    
 }
