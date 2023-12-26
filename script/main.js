2d24b9f7e555fc07a28018555f70e79fe0522a1e
Chilufya <64278891+cmmutale@users.noreply.github.com>
2023-12-26 13:55:36 -0600

    Implemented a responsive grid gallery that gets images from a json file (#56)
    * Implemented a responsive grid gallery that gets images from a json file
    
    * added image gallery to activities page

--- script/main.js
+++ script/main.js
@@ -10,24 +10,24 @@ var navbarlist = document.querySelector('#navbar ul.mobileNav')
 
 window.onload = () => {
     console.log(prefLanguage);
-    if(prefLanguage != 'en')
+    if (prefLanguage != 'en')
         changelanguage();
- }
+}
 
-function changelanguage(){
-    prefLanguage = prefLanguage == 'en'? 'hi': 'en';
+function changelanguage() {
+    prefLanguage = prefLanguage == 'en' ? 'hi' : 'en';
     htmlParent.lang = prefLanguage;
     translateElements.forEach(el => swap(el));
-   /*
-    prefLanguage = lang;
-    if(prefLanguage == 'en'){
-        makeEnglish();
-    }
-    else{
-        makeHindi();
-    }
-    console.log('current pref ',lang)
-    */
+    /*
+     prefLanguage = lang;
+     if(prefLanguage == 'en'){
+         makeEnglish();
+     }
+     else{
+         makeHindi();
+     }
+     console.log('current pref ',lang)
+     */
 }
 
 function makeEnglish() {
@@ -42,18 +42,50 @@ function makeHindi() {
     //englishElements.forEach(el => swap(el));
 }
 
-function swap(el){
+function swap(el) {
     var i = el.ariaLabel || el.innerText;
     el.ariaLabel = el.innerText;
     el.innerText = i;
-    
+
 }
 
 function toggleMobileNav() {
-    if(navbarlist.className==='mobileNav'){
-        navbarlist.className+=" mobileVisible"
+    if (navbarlist.className === 'mobileNav') {
+        navbarlist.className += " mobileVisible"
     }
-    else{
-        navbarlist.className='mobileNav';
+    else {
+        navbarlist.className = 'mobileNav';
     }
 }
+
+// PULL IMAGES FROM 'imges.json' AND DISPLAY THEM
+document.addEventListener("DOMContentLoaded", function () {
+    const gallery = document.getElementById("gallery");
+
+    // Fetch JSON data
+    fetch("images.json")
+        .then(response => response.json())
+        .then(data => {
+            // Iterate through each image in the JSON
+            data.images.forEach(image => {
+                // Create elements for image and caption
+                const item = document.createElement("div");
+                item.classList.add("gallery-item");
+
+                const img = document.createElement("img");
+                img.src = image.url;
+                img.alt = image.caption;
+
+                const caption = document.createElement("p");
+                caption.textContent = image.caption;
+
+                // Append elements to the gallery
+                item.appendChild(img);
+                item.appendChild(caption);
+                gallery.appendChild(item);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
+});
