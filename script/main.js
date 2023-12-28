b40482e5f202b24becb25e79af66aac982423d51
Aditya Raute <43912470+adityaraute@users.noreply.github.com>
2023-12-28 06:25:14 +0000

    Prettier workflow merging (#62)
    * Create main.yml
    
    * Sample commit
    
    * Update and rename main.yml to .github/workflows/main.yml
    
    * Update and rename main.yml to prettify.yml
    
    * Update prettify.yml
    
    * Update the git command
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: adityaraute <adityaraute@users.noreply.github.com>

--- script/main.js
+++ script/main.js
@@ -1,45 +1,45 @@
-var prefLanguage = 'en';
+var prefLanguage = "en";
 //https://stackoverflow.com/questions/16206322/how-to-get-js-variable-to-retain-value-after-page-refresh
 
-var translateElements = document.querySelectorAll('.translate')
-var htmlParent = document.querySelector('html')
-var navbarlist = document.querySelector('#navbar ul.mobileNav')
+var translateElements = document.querySelectorAll(".translate");
+var htmlParent = document.querySelector("html");
+var navbarlist = document.querySelector("#navbar ul.mobileNav");
 
 //var englishElements = document.querySelectorAll('.translate')
 //var hindiElements = document.querySelectorAll('[lang = hi]')
 
 window.onload = () => {
-    console.log(prefLanguage);
-    if (prefLanguage != 'en')
-        changelanguage();
-}
+  console.log(prefLanguage);
+  if (prefLanguage != "en") changelanguage();
+};
 
 function changelanguage() {
-    prefLanguage = prefLanguage == 'en' ? 'hi' : 'en';
-    htmlParent.lang = prefLanguage;
-    translateElements.forEach(el => swap(el));
-    /*
-     prefLanguage = lang;
-     if(prefLanguage == 'en'){
-         makeEnglish();
-     }
-     else{
-         makeHindi();
-     }
-     console.log('current pref ',lang)
-     */
+  prefLanguage = prefLanguage == "en" ? "hi" : "en";
+  htmlParent.lang = prefLanguage;
+  translateElements.forEach((el) => swap(el));
+  /*
+    prefLanguage = lang;
+    if(prefLanguage == 'en'){
+        makeEnglish();
+    }
+    else{
+        makeHindi();
+    }
+    console.log('current pref ',lang)
+    */
+
 }
 
 function makeEnglish() {
-    //hindiElements.forEach(el => el.style.display = "none");
-    //englishElements.forEach(el => el.style.display = "unset");
+  //hindiElements.forEach(el => el.style.display = "none");
+  //englishElements.forEach(el => el.style.display = "unset");
 }
 
 function makeHindi() {
-    //hindiElements.forEach(el => el.style.display = "unset");
-    //console.log(englishElements);
-    //englishElements.forEach(el => el.style.display = "none");
-    //englishElements.forEach(el => swap(el));
+  //hindiElements.forEach(el => el.style.display = "unset");
+  //console.log(englishElements);
+  //englishElements.forEach(el => el.style.display = "none");
+  //englishElements.forEach(el => swap(el));
 }
 
 function swap(el) {
