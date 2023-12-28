f6b84f0b3245ffdb5043aebb28bf80caf6e8ff25
adityaraute <adityaraute@users.noreply.github.com>
2023-12-28 06:18:10 +0000

    Prettified Code!

--- script/main.js
+++ script/main.js
@@ -1,24 +1,23 @@
-var prefLanguage = 'en';
+var prefLanguage = "en";
 //https://stackoverflow.com/questions/16206322/how-to-get-js-variable-to-retain-value-after-page-refresh
 
-var translateElements = document.querySelectorAll('.translate')
-var htmlParent = document.querySelector('html')
-var navbarlist = document.querySelector('#navbar ul.mobileNav')
+var translateElements = document.querySelectorAll(".translate");
+var htmlParent = document.querySelector("html");
+var navbarlist = document.querySelector("#navbar ul.mobileNav");
 
 //var englishElements = document.querySelectorAll('.translate')
 //var hindiElements = document.querySelectorAll('[lang = hi]')
 
 window.onload = () => {
-    console.log(prefLanguage);
-    if(prefLanguage != 'en')
-        changelanguage();
- }
+  console.log(prefLanguage);
+  if (prefLanguage != "en") changelanguage();
+};
 
-function changelanguage(){
-    prefLanguage = prefLanguage == 'en'? 'hi': 'en';
-    htmlParent.lang = prefLanguage;
-    translateElements.forEach(el => swap(el));
-   /*
+function changelanguage() {
+  prefLanguage = prefLanguage == "en" ? "hi" : "en";
+  htmlParent.lang = prefLanguage;
+  translateElements.forEach((el) => swap(el));
+  /*
     prefLanguage = lang;
     if(prefLanguage == 'en'){
         makeEnglish();
@@ -31,29 +30,27 @@ function changelanguage(){
 }
 
 function makeEnglish() {
-    //hindiElements.forEach(el => el.style.display = "none");
-    //englishElements.forEach(el => el.style.display = "unset");
+  //hindiElements.forEach(el => el.style.display = "none");
+  //englishElements.forEach(el => el.style.display = "unset");
 }
 
 function makeHindi() {
-    //hindiElements.forEach(el => el.style.display = "unset");
-    //console.log(englishElements);
-    //englishElements.forEach(el => el.style.display = "none");
-    //englishElements.forEach(el => swap(el));
+  //hindiElements.forEach(el => el.style.display = "unset");
+  //console.log(englishElements);
+  //englishElements.forEach(el => el.style.display = "none");
+  //englishElements.forEach(el => swap(el));
 }
 
-function swap(el){
-    var i = el.ariaLabel || el.innerText;
-    el.ariaLabel = el.innerText;
-    el.innerText = i;
-    
+function swap(el) {
+  var i = el.ariaLabel || el.innerText;
+  el.ariaLabel = el.innerText;
+  el.innerText = i;
 }
 
 function toggleMobileNav() {
-    if(navbarlist.className==='mobileNav'){
-        navbarlist.className+=" mobileVisible"
-    }
-    else{
-        navbarlist.className='mobileNav';
-    }
+  if (navbarlist.className === "mobileNav") {
+    navbarlist.className += " mobileVisible";
+  } else {
+    navbarlist.className = "mobileNav";
+  }
 }
